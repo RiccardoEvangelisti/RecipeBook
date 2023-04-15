c2501311499f941e0a3ea4be5084a5a76781684d
RiccardoEvangelisti <evangericcardo00@gmail.com>
2023-04-15 19:24:47 +0200

    Inital changes

--- app/src/main/java/com/projects/android/recipebook/utils/TakePictureUtils.kt
+++ app/src/main/java/com/projects/android/recipebook/utils/TakePictureUtils.kt
@@ -1,12 +1,15 @@
 package com.projects.android.recipebook.utils
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.projects.android.recipebook.Manifest
 
 class TakePictureUtils {
 	companion object {
@@ -15,7 +18,7 @@ class TakePictureUtils {
 
 		fun askPermission(activity: Activity) {
 			if (allPermissionsGranted(activity.baseContext)) {
-				startCamera()
+				startCamera(activity.baseContext)
 			} else {
 				ActivityCompat.requestPermissions(
 					activity, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
@@ -25,6 +28,34 @@ class TakePictureUtils {
 
 		private fun allPermissionsGranted(baseContext: Context) = REQUIRED_PERMISSIONS.all {
 			ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+		}
+
+		private fun startCamera(baseContext: Context) {
+			val cameraProviderFuture = ProcessCameraProvider.getInstance(baseContext)
+
+			cameraProviderFuture.addListener({
+				// Used to bind the lifecycle of cameras to the lifecycle owner
+				val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+				// Preview
+				val preview = Preview.Builder().build().also {
+					it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+				}
+
+				// Select back camera as a default
+				val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+				try {
+					// Unbind use cases before rebinding
+					cameraProvider.unbindAll()
+
+					// Bind use cases to camera
+					cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+				} catch (exc: Exception) {
+					Log.e("RecipeBook", "Use case binding failed", exc)
+				}
+
+			}, ContextCompat.getMainExecutor(this))
 		}
 	}
 }
